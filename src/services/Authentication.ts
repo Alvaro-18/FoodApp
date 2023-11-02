13ce41c629dcaf4cd27c9688424d1687ec22ebed
Álvaro Marques <alvaromarques18@gmail.com>
2023-11-02 18:24:53 -0300

    feat: add requests

---
+++ src/services/Authentication.ts
@@ -0,0 +1,30 @@
+import axios from "axios";
+
+const key = {
+  key_firebase: "AIzaSyDOA8AwvPNvoIOvVAZaGUGL2deEOec44Vo"
+};
+
+export async function createUser(name:string, email:string, password:string){
+  const response = await axios.post(`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${key.key_firebase}`, {
+   displayName:name,
+   email: email,
+   password:password,
+ });
+}
+
+export async function logUser(email:string, password:string) {
+ const response = await axios.post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${key.key_firebase}`,{
+   email: email,
+   password:password,
+ });
+
+ //await AsyncStorage.setItem("username", response.data.displayName);
+// await AsyncStorage.setItem("token", response.data.idToken);
+}
+
+export async function resetPassword(email:string){
+ const response = await axios.post(`https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=${key.key_firebase}`, {
+   requestType: "PASSWORD_RESET",
+   email: email,
+ });
+}
