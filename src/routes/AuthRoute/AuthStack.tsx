eaca4d6ff94d75c4e195337a7589bf89e61478c0
Álvaro Marques <alvaromarques18@gmail.com>
2023-11-03 04:02:09 -0300

    feat: add stack navigator

---
+++ src/routes/AuthRoute/AuthStack.tsx
@@ -0,0 +1,17 @@
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import {LoginScreen} from "../../screens/LoginScreen";
+import {SignUpScreen} from "../../screens/SignUpScreen";
+import {ForgotPasswordScreen} from "../../screens/ForgotPasswordScreen";
+
+
+const Stack = createNativeStackNavigator();
+
+export function AuthStack() {
+  return (
+    <Stack.Navigator screenOptions={{headerShown: false}}>
+      <Stack.Screen name="SignUp" component={SignUpScreen} />
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
+    </Stack.Navigator>
+  );
+}
