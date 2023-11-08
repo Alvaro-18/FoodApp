de06a441f473d81e9f90f63f5df04be98b40bec2
Álvaro Marques <alvaromarques18@gmail.com>
2023-11-08 02:32:59 -0300

    feat: add mocked data to test

---
+++ src/store/Data.ts
@@ -0,0 +1,115 @@
+import { Option } from "../types/interfaces/Option";
+import { Notification } from "../types/interfaces/Notification";
+import { Product } from "../types/interfaces/Product";
+
+export const ADDRESS:Option[] = [
+ {
+    isSelected: false,
+    title: "Home",
+    subtitle1: "Address: Vitória da Conquista",
+    subtitle2: "City: Vitória da Conquista"
+  },
+ {
+    isSelected: false,
+    title: "Work",
+    subtitle1: "Address: 1600 Amphitheatre Parkway",
+    subtitle2: "City: Mountain View"
+  },
+ {
+    isSelected: true,
+    title: "University",
+    subtitle1: "Address: Av. Sérgio Vieira de Melo, 3150",
+    subtitle2: "City: Vitória da Conquista"
+  },
+];
+
+export const NOTIFICATIONS:Notification[] = [
+  {
+    title: "Notification",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.",
+    isRead: false
+  },
+  {
+    title: "Notification2",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.",
+    isRead: true
+  },
+  {
+    title: "Notification3",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.",
+    isRead: true
+  },
+  {
+    title: "Notification4",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.",
+    isRead: true
+  },
+  {
+    title: "Notification5",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.",
+    isRead: true
+  },
+  {
+    title: "Notification5",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.",
+    isRead: true
+  },
+  {
+    title: "Notification5",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.",
+    isRead: true
+  },
+  {
+    title: "Notification5",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.",
+    isRead: true
+  },
+  {
+    title: "Notification5",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.",
+    isRead: true
+  },
+];
+
+export const  PRODUCTS:Product[] = [
+  {
+    id: "1",
+    productImageUrl: "https://img.freepik.com/fotos-gratis/xicara-de-cafe-latte-quente_1339-8381.jpg?w=360&t=st=1699337551~exp=1699338151~hmac=3a5de69c3afaa55150bf7662d9706715fcb3d56334fbed49815fea92646a1742",
+    productName: "Coffee with milk",
+    productDescription: "lorem",
+    productPrice: "12",
+    store: "Starbucks"
+  },
+  {
+    id: "2",
+    productImageUrl: "https://img.freepik.com/fotos-gratis/xicara-de-cafe-latte-quente_1339-8381.jpg?w=360&t=st=1699337551~exp=1699338151~hmac=3a5de69c3afaa55150bf7662d9706715fcb3d56334fbed49815fea92646a1742",
+    productName: "Coffee with milk",
+    productDescription: "lorem",
+    productPrice: "12",
+    store: "Starbucks"
+  },
+  {
+    id: "3",
+    productImageUrl: "https://img.freepik.com/fotos-gratis/xicara-de-cafe-latte-quente_1339-8381.jpg?w=360&t=st=1699337551~exp=1699338151~hmac=3a5de69c3afaa55150bf7662d9706715fcb3d56334fbed49815fea92646a1742",
+    productName: "Coffee with milk",
+    productDescription: "lorem",
+    productPrice: "12",
+    store: "Starbucks"
+  },
+  {
+    id: "4",
+    productImageUrl: "https://img.freepik.com/fotos-gratis/xicara-de-cafe-latte-quente_1339-8381.jpg?w=360&t=st=1699337551~exp=1699338151~hmac=3a5de69c3afaa55150bf7662d9706715fcb3d56334fbed49815fea92646a1742",
+    productName: "Coffee with milk",
+    productDescription: "lorem",
+    productPrice: "12",
+    store: "Starbucks"
+  },
+  {
+    id: "5",
+    productImageUrl: "https://img.freepik.com/fotos-gratis/xicara-de-cafe-latte-quente_1339-8381.jpg?w=360&t=st=1699337551~exp=1699338151~hmac=3a5de69c3afaa55150bf7662d9706715fcb3d56334fbed49815fea92646a1742",
+    productName: "Coffee with milk",
+    productDescription: "lorem",
+    productPrice: "12",
+    store: "Starbucks"
+  },
+];
