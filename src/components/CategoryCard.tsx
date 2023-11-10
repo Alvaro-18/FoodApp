65a4383a50785edf3e0713cd9e0bc5bab7f9053a
Álvaro Marques <alvaromarques18@gmail.com>
2023-11-10 02:53:06 -0300

    feat: add category card

---
+++ src/components/CategoryCard.tsx
@@ -0,0 +1,48 @@
+import {Pressable, Image, StyleSheet, GestureResponderEvent, Text} from "react-native";
+
+export function CategoryCard({onPress, title} : {onPress: (event: GestureResponderEvent) => void, title:string}) {
+  return (
+    <Pressable onPress={onPress} style={styles.button}>
+      <Image
+        source={require("../assets/images/Coffe.jpg")}
+        resizeMode={"contain"}
+        style={styles.image}
+      />
+      <Text style={styles.text}>{title}</Text>
+    </Pressable>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    width: 78,
+    height: 90,
+    borderRadius:12,
+    backgroundColor:"#fff",
+    marginHorizontal: 4,
+    alignItems: "center",
+    justifyContent: "center",
+    marginVertical: 10,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.29,
+    shadowRadius: 4,
+    elevation: 7,
+  },
+
+  image:  {
+    width: 46,
+    height: 46,
+    borderRadius: 50,
+    marginBottom: 8
+  },
+
+  text:{
+    color: "#000",
+    fontSize: 16,
+    fontWeight: "500"
+  }
+});
