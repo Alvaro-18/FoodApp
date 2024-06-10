d93cc9bdaa86de30dae29c16b93bcb79d025c3ab
Álvaro Marques <alvaromarques18@gmail.com>
2024-06-09 23:18:27 -0300

    refactor: refactor enum

--- src/screens/OrdersScreen.tsx
+++ src/screens/OrdersScreen.tsx
@@ -2,12 +2,13 @@ import {View, StyleSheet} from "react-native";
 import {HeaderBar} from "../components/home/HeaderBar";
 import {PRODUCTS} from "../store/Data";
 import { OrderCard } from "../components/order/OrderCard";
+import { Status } from "../types/enums/Status";
 
 export function OrdersScreen() {
   return (
     <View style={styles.container}>
       <HeaderBar />
-      <OrderCard data={PRODUCTS[0]} status="COMPLETED"/>
+      <OrderCard data={PRODUCTS[0]} status={Status.COMPLETED}/>
     </View>
   );
 }
