31f818c167d8a29b2c8fed0f71a437f21f414a87
Álvaro Marques <alvaromarques18@gmail.com>
2024-07-05 15:32:31 -0300

    feat: add header bar

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -3,14 +3,12 @@ import {View, StyleSheet, FlatList} from "react-native";
 import {CategoriesSection} from "../components/home/CategoriesSection";
 import {CATEGORIES, STORE} from "../store/Data";
 import {StoreSection} from "../components/home/StoreSection";
-import {HeaderBar} from "../components/home/HeaderBar";
 
 export function HomeScreen() {
   return (
     <View style={styles.container}>
-      <HeaderBar />
+   
      <FlatList
-        style={styles.list}
         ListHeaderComponent={() => <CategoriesSection data={CATEGORIES} />}
         data={CATEGORIES}
         renderItem={({item}) => (
@@ -26,10 +24,8 @@ const styles = StyleSheet.create({
   container: {
     alignSelf: "center",
     width: "95%",
-    marginTop: "6%",
+
   },
 
-  list: {
-    marginBottom: "14%",
-  },
+
 });
